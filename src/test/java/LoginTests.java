cfc71c66b250b6b991113123b18f6565c494c97a
adamnall1991 <adam.nall@testpro.io>
2025-11-21 14:36:21 -0500

    test comment at the end

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -23,5 +23,6 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+        //test comment
     }
 }
