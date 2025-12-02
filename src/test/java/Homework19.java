c009248f2edde76bf52419adb82d583cad7b9aae
adamnall1991 <adam.nall@testpro.io>
2025-12-01 22:17:22 -0500

    Submission for Homework 19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class Homework19 extends BaseTest {
+    @Test
+    public void deletePlaylist() throws InterruptedException{
+        String expectedPlaylistDeletedMessage = "Deleted playlist \"AdamTest1.\"";
+
+        navigateToPage();
+        provideEmail("adam.nall@testpro.io");
+        providePassword("adam_test1");
+        clickSubmit();
+        openPlaylist();
+        clickDeletePlaylistBtn();
+        Assert.assertEquals(getDeletedPlaylistMsg(), expectedPlaylistDeletedMessage);
+
+    }
+
+    public void openPlaylist(){
+        WebElement emptyPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        emptyPlaylist.click();
+    }
+
+    public void clickDeletePlaylistBtn() throws InterruptedException{
+        WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deletePlaylist.click();
+        Thread.sleep(2000);
+    }
+
+    public String getDeletedPlaylistMsg(){
+        WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationMsg.getText();
+    }
+
+
+}
